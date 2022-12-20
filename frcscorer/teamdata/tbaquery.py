e625f13872d0583ea0a588c24dd84c7ed92e323b
Darin Huang <huangdarin465@gmail.com>
2022-12-20 23:25:04 +1100

    query status and simple teams from tba

---
+++ frcscorer/teamdata/tbaquery.py
@@ -0,0 +1,53 @@
+import requests
+from dotenv import load_dotenv
+import os
+import json
+import logging
+
+TBA_BASE_URL = 'https://www.thebluealliance.com/api/v3/'
+
+load_dotenv()
+HEADER = {'X-TBA-Auth-Key': os.environ['TBA_Auth_Key']}
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    status_code = status()
+    if status_code != 200:
+        logging.error('TBA status did not return 200')
+        return
+    teamlist = teams()
+    print(len(teamlist))
+    print('Done')
+
+
+def status():
+    res = requests.get(TBA_BASE_URL + 'status', headers=HEADER)
+    return res.status_code
+
+
+def teams():
+    teamlist = list()
+    page_num = 0
+    logging.debug(f'GET {TBA_BASE_URL}teams/{page_num}/simple')
+    res = requests.get(
+        TBA_BASE_URL + f'teams/{page_num}/simple', headers=HEADER)
+    logging.debug(f'Response status code: {res.status_code}')
+
+    while res.status_code == 200:
+        data = json.loads(res.text)
+        if len(data) == 0:
+            break
+        teamlist.extend(data)
+        logging.info(f'Extending team list (length: {len(teamlist)})')
+        page_num = page_num + 1
+        logging.debug(f'GET {TBA_BASE_URL}teams/{page_num}/simple')
+        res = requests.get(
+            TBA_BASE_URL + f'teams/{page_num}/simple', headers=HEADER)
+        logging.debug(f'Response status code: {res.status_code}')
+
+    return teamlist
+
+
+if __name__ == '__main__':
+    main()
