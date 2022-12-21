380abf8c467d65c65683b86b2feaaef31f766713
Darin Huang <huangdarin465@gmail.com>
2022-12-22 01:18:09 +1100

    can add all teams to fauna

--- frcscorer/frcscorer.py
+++ frcscorer/frcscorer.py
@@ -1,3 +1,11 @@
 from tba import query
+from teamdata import teamdata
+
 
 print(query.status())
+
+teams = query.teams()
+
+teamdata.create_team_data(teams, 'TeamsSimple')
+
+print('Done')
--- frcscorer/teamdata/teamdata.py
+++ frcscorer/teamdata/teamdata.py
@@ -2,23 +2,27 @@ from faunadb.client import FaunaClient
 from faunadb import query as q
 from dotenv import load_dotenv
 import os
+import logging
+from tqdm import tqdm
 
 load_dotenv()
 
 client = FaunaClient(secret=os.environ['FAUNA_SECRET_KEY'])
 
-# res = client.ping()
 
-# res = client.query(q.get(q.ref(q.collection('Test'), "347563724815467092")))
-
-# ref = client.query(q.select("ref", q.get(q.collection("AllTime"))))
-
-# res = client.query(q.get(q.select('ref', q.get(q.collection('AllTime')))))
-
-# res = client.query(q.get(q.collection('AllTime')))
-
-# res = client.query(q.paginate(q.collection('AllTime')))
-
-res = client.query(q.paginate(q.documents(q.collection('AllTime'))))
-
-print(res)
+def create_team_data(teams: list, collection: str = 'TeamsSimple'):
+    '''
+    Creates the team data in the FaunaDB database.
+    Accepts in the list of teams from TBA.
+    '''
+    def to_data_obj(team):
+        return {
+            'data': team
+        }
+    team_data = list(map(to_data_obj, teams))
+    logging.debug('Mapping list to data objects')
+    for team in tqdm(team_data):
+        res = client.query(
+            q.create(q.collection(collection_name=collection), team))
+        ref = res['ref']
+        logging.debug(f'Created document reference: {ref}')
