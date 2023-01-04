d3c13ffc1e9ea7205598909e8cad46cea6c2e2e7
Darin Huang <huangdarin465@gmail.com>
2023-01-04 17:33:49 +1100

    Got rid of unnecessary lines

--- frcscorer/ratings/alltime.py
+++ frcscorer/ratings/alltime.py
@@ -31,16 +31,10 @@ class AllTimeRating:
                 rat = Rating()
                 self.teamratings[tk] = rat
             red_curr_ratings.append(rat)
-        # blue_curr_ratings = [self.teamratings[tk] for tk in bluekeys]
-        # red_curr_ratings = [self.teamratings[tk] for tk in redkeys]
         if match['winning_alliance'] == 'blue':
-            # [[b1, b2, b3], [r1, r2, r3]] = rate(
-            #     [blue_curr_ratings, red_curr_ratings])
             [bluenew, rednew] = rate(
                 [blue_curr_ratings, red_curr_ratings])
         else:
-            # [[r1, r2, r3], [b1, b2, b3]] = rate(
-            #     [red_curr_ratings, blue_curr_ratings])
             [rednew, bluenew] = rate(
                 [red_curr_ratings, blue_curr_ratings])
 
@@ -48,9 +42,3 @@ class AllTimeRating:
             self.teamratings[bluekeys[i]] = r
         for i, r in enumerate(rednew):
             self.teamratings[redkeys[i]] = r
-        # self.teamratings[bluekeys[0]] = b1
-        # self.teamratings[bluekeys[1]] = b2
-        # self.teamratings[bluekeys[2]] = b3
-        # self.teamratings[redkeys[0]] = r1
-        # self.teamratings[redkeys[1]] = r2
-        # self.teamratings[redkeys[2]] = r3
